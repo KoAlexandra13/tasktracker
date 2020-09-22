a0c1def8a55c162c8eee5a6d7503e261d802f61e
asenyarb <dimoniss00@gmail.com>
2020-09-22 22:38:20 +0300

    Add Sign Up and Log In views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from rest_framework.views import status, Response
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import decorators
+from rest_framework.permissions import AllowAny
 
 from api.models import (
     User, Table
@@ -7,15 +9,32 @@ from api.models import (
 from api.serializers import (
     UserDetailSerializer, TableDetailSerializer
 )
+from api.permissions import IsSuperUserOrOwner
+from api.forms import UserCreationForm
 
 
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserDetailSerializer
-    permission_classes = []
+    permission_classes = [IsSuperUserOrOwner]
+
+    def create(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    @decorators.permission_classes([AllowAny])
+    @decorators.action(detail=False, methods=['post'], url_path='sign_up')
+    def sign_up(self, request, *args, **kwargs):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(form.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @decorators.action(detail=False, methods=['get'], url_path='self')
+    def get_self(self, request, *args, **kwargs):
+        return Response(UserDetailSerializer(request.user).data, status=status.HTTP_200_OK)
 
 
 class TableViewSet(ModelViewSet):
     queryset = Table.objects.all()
     serializer_class = TableDetailSerializer
-    permission_classes = []
